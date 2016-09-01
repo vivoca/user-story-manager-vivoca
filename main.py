308ccd99bf81b5f8e8e821a2848a2d0cc205d502
egyud_v <vivien.egyud@gmail.com>
2016-09-01 13:52:18 +0200

    display added user stories in /list

--- main.py
+++ main.py
@@ -26,5 +26,57 @@ def story_edit():
     pass
 
 
+@app.route('/list')
+def story_list():
+    adding_table_to_html()
+    return render_template("list.html")
+
+
+def print_table():
+    l = UserStory.get_userstory_list()
+    html = """
+    <!doctype html>
+
+    <html>
+
+    <head>
+    <title>User Story Manager</title>
+    </head>
+
+    <body>
+        <h1>User Story Manager</h1>
+    <table border='1'>
+        <thead>
+        <tr><th colspan="7">User Story Manager</th></tr>
+        <tr><th>Story ID</th><th>Story Title</th>
+            <th>User Story</th><th>Acceptance Criteria</th>
+            <th>Business Value</th><th>Estimation</th>
+            <th>Status</th>
+        </tr>
+        </thead>
+
+        <tbody>
+    """
+    for i in l:
+        html += ('<tr>\n')
+        for j in range(7):
+            html += (('<td>') + str(i[j]) + ('</td>'))
+        html += ('</tr>\n')
+    html += """</tbody>
+
+    </table>
+    </body>
+    </html>
+    """
+    return html
+
+
+def adding_table_to_html():
+    html = print_table()
+    html_file = open("templates/list.html", "w")
+    html_file.write(html)
+    html_file.close
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -27,3 +27,13 @@ class UserStory(BaseModel):
     business_value = IntegerField()
     estimation = FloatField()
     story_status = CharField()
+
+    @classmethod
+    def get_userstory_list(cls):
+        list1, userstory_list = [], []
+        r = list(cls.select(cls))
+        for element in r:
+            list1 = [element.id, element.story_title, element.user_story, element.acceptance_criteria,
+                     element.business_value, element.estimation, element.story_status]
+            userstory_list.append(list1)
+        return userstory_list
